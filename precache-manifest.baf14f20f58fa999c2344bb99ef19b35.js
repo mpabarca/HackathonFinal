fa3a6d4815f90e1c54b6ce130c1845205d5f52d6
mpabarca <mpabarca@uc.cl>
2019-05-17 18:11:45 -0400

    Updates

---
+++ precache-manifest.baf14f20f58fa999c2344bb99ef19b35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569f347daab1014c2c230aeed26c1b65",
+    "url": "/HackathonFinal/index.html"
+  },
+  {
+    "revision": "1b0274da47f4d345dc5a",
+    "url": "/HackathonFinal/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/HackathonFinal/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1b0274da47f4d345dc5a",
+    "url": "/HackathonFinal/static/js/main.1657768c.chunk.js"
+  },
+  {
+    "revision": "eaeada003c74e9c18bb6",
+    "url": "/HackathonFinal/static/js/runtime~main.a07b57a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HackathonFinal/static/media/logo.5d5d9eef.svg"
+  }
+]);
