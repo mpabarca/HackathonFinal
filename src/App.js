1cc191c3670e12a9189138a90dfb033802d3fd76
mpabarca <mpabarca@uc.cl>
2019-05-21 20:55:08 -0400

    se agrega componente ownlocation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './components/Maps/Map';
+import OwnLocation from './components/Maps/OwnLocation';
+import {companies} from './companies.json'
 
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.map=null;
     this.state = {
         image: 'normal.day',
         latitude: false,
         longitude: false,
-        error: null
+        error: null,
+        value:'0,0', 
+        companies: companies
     }
   }
-
     
 
     render() {
         return (
             <div className="App">
-                <Map 
+                <OwnLocation
                     app_id="SqgXt9Xu4ZtrdyRXBAHw"
                     app_code="4_H5feYpb2trd0PaEdD_bQ"
-                    zoom="10"
-                    image={this.state.image }/>
+                    zoom="13"
+                    image={this.state.image }
+                    listCompany={this.state.companies}
+                />
+                
+                
             </div>
         );
     }
@@ -31,3 +39,12 @@ class App extends Component {
 
 
 export default App;
+
+/*
+                <Map 
+                    app_id="SqgXt9Xu4ZtrdyRXBAHw"
+                    app_code="4_H5feYpb2trd0PaEdD_bQ"
+                    zoom="10"
+                    image={this.state.image }
+                />
+*/
---
+++ src/components/Maps/OwnLocation.js
@@ -0,0 +1,146 @@
+import React, {Component} from 'react';
+
+class OwnLocation extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.platform = null;
+        this.map = null;
+
+        this.state = {
+            app_id: props.app_id,
+            app_code: props.app_code,
+            position: null,
+            zoom: props.zoom,
+            map: null,
+            image: props.image,
+            style: props.style,
+        }
+    }
+
+    getPlatform() {return new window.H.service.Platform(this.state);}
+    getMap(container, layers, settings) {return new window.H.Map(container, layers, settings);}
+    getEvents(map) {return new window.H.mapevents.MapEvents(map);}
+    getUI(map, layers) {return new window.H.ui.UI.createDefault(map, layers);}
+
+    componentDidMount() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    this.map.setCenter({
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    });
+                    var ownMarker = new window.H.map.Marker({lat:position.coords.latitude, lng:position.coords.longitude});
+                    (this.map).addObject(ownMarker);
+
+                    var stylePto = {fillColor: 'rgba(35, 51, 129, 0.3)',lineWidth: 5,strokeColor: 'rgba(114, 38, 51, 1)'};
+                    var circle = new window.H.map.Circle(
+                    new window.H.geo.Point(position.coords.latitude, position.coords.longitude),1000,{stylePto: stylePto} );
+                    circle.setData('Circle');
+                    var container = new window.H.map.Group({objects: [circle]});
+                    this.map.addObject(container);
+
+                },
+                (error) => this.setState({
+                    error: error.message
+                })
+            );
+        }
+        //aqui se llaman todos los estados
+        this.platform = this.getPlatform();
+        //aqui se configura el tipo de mapa
+        var layers = this.platform.createDefaultLayers();
+        //con element renderizamos el mapa
+        var element = document.getElementById('here-map');
+        //muestra mapa de acuerdo a mi ubicacion
+        this.map = this.getMap(element, layers.normal.map, {
+            center: this.state.center,
+            zoom: this.state.zoom,
+        }); 
+        //con esto puedo hacer zoom 
+        new window.H.mapevents.Behavior(new window.H.mapevents.MapEvents(this.map));
+      
+       this.getUI(this.map, layers);
+        this.setState({
+            ...this.state,
+            map: this.map
+        });
+        
+        
+        // create default UI with layers provided by the platform
+        var ui = window.H.ui.UI.createDefault(this.map, layers); 
+
+        function addMarkerToGroup(map, group, coord, html) {
+            var svgMarkup = '<svg width="24" height="24" ' +
+            'xmlns="http://www.w3.org/2000/svg">' +
+            '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+            'height="22" /><text x="12" y="18" font-size="12pt" ' +
+            'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+            'fill="white">H</text></svg>'
+            var icon = new window.H.map.Icon(svgMarkup),
+            marker = new window.H.map.Marker({lat:coord[0], lng:coord[1]}, {icon: icon});
+            map.addObject(marker);
+
+            // add custom data to the marker
+            marker.setData(html);
+            group.addObject(marker);
+        }
+        
+        function addInfoBubble(map, coord, name) {
+            var group = new window.H.map.Group();
+            map.addObject(group);
+            // add 'tap' event listener, that opens info bubble, to the group
+            group.addEventListener('tap', function (e) {
+            // event target is the marker itself, group is a parent event target
+            // for all objects that it contains
+            var bubble =  new window.H.ui.InfoBubble(e.target.getPosition(), {
+                // read custom data
+                content: e.target.getData()
+            });
+            // show info bubble
+            ui.addBubble(bubble);
+            }, false);
+            
+            addMarkerToGroup(map, group, coord,'<div><a href=\'http://www.mcfc.co.uk\' >${name}</a>');
+        }
+         Object.keys(this.props.listCompany).map(key=>(
+            addInfoBubble(this.map, this.props.listCompany[key].located, this.props.listCompany[key].name )
+          
+        ));
+          
+    
+
+}
+
+shouldComponentUpdate(props, state) {
+    this.changeTheme(props.image, props.style);
+    return false;
+}
+
+//Cree una instancia de MapTileService para solicitar mosaicos base (es decir, base.map.api.here.com
+//aqui especificamos el estilo del mapa y como se mostrara
+changeTheme(image, style) {
+    var tiles = this.platform.getMapTileService({
+        'type': 'base'
+    });
+    var layer = tiles.createTileLayer(
+        'maptile',
+        image,
+        256,
+        'png', {
+            'style': 'flame'
+        }
+    );
+    this.map.setBaseLayer(layer);
+}
+    render(){
+        return(
+            <div id = "here-map"
+                style = {{ width: '360px', height: '640px', background: 'grey'}}
+            />
+        )
+    }
+}
+export default OwnLocation;
