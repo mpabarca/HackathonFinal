80a012093fa02ebf4f65cac6c210f71d430a081f
mpabarca <mpabarca@uc.cl>
2019-05-23 10:03:21 -0400

    diseño registro y medio diseño empresas perfil

--- src/components/CompanyProfile.js
+++ src/components/CompanyProfile.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './companyprofile.css';
 import {Container, Row, Col, Button, Image } from 'react-bootstrap';
 import profileCompany from './img/profile-company.png';
+import tela from './img/tela-letra.png';
 
 class CompanyProfile extends Component{
 
@@ -11,29 +12,34 @@ class CompanyProfile extends Component{
                 <Row id="back"><i id="icon-back" class="fas fa-chevron-left"></i>BACK</Row>
                 <Row id="row-tittle">
                     <Col id="tittle">Textil Sumey</Col>
-                    <Col><Image id="profile" src={profileCompany} roundedCircle /></Col>
+                    <Col>
+                        <Image id="profile" src={profileCompany} roundedCircle />
+                        <div id="row-text">
+                            <Row id="mayorista" className="text">Mayorista textil en</Row>
+                            <Row id="recoleta" className="text">Recoleta, Chile</Row>
+                        </div>
+                    </Col>
                 </Row>
-                <div id="row-text">
-                    <Row className="text">Mayorista textil en</Row>
-                    <Row className="text">Recoleta, Chile</Row>
-                </div>
+                
                 
                 <Row id="row-type">
-                    <Col id="icon-type"><i class="fas fa-ellipsis-v"/><i class="fas fa-ellipsis-v"/><i class="fas fa-ellipsis-v"/></Col>
-                    <Col>
-                        <Row className="text">Retazos</Row>
-                        <Row className="text">Cuero</Row>
-                    </Col>
-                    <Col><i class="fas fa-user"></i></Col>
+                    <div id="col-info">Material reutilizable disponible</div>
+                    <Col> <Image id="tela-icon" src={tela}/></Col>
                 </Row>
-                <Row>Horarios</Row>
-                <Row>
-                    <Col>L</Col>
-                    <Col>M</Col>
-                    <Col>W</Col>
-                    <Col>J</Col>
-                    <Col>V</Col>
-                    <Col>S</Col>
+                <Row id="horario">Horarios</Row>
+                <Row id="row-horario">
+                    <Col className="day">L</Col>
+                    <Col className="day">M</Col>
+                    <Col className="day">W</Col>
+                    <Col className="day">J</Col>
+                    <Col className="day">V</Col>
+                    <Col className="day">S</Col>
+                </Row>
+                <Row id="five">Entre 17:00 - 18:00</Row>
+                <Row id="row-info">
+                    <div>Dirección</div>
+                    <div>Antonia López de Bello 285, Recoleta</div>
+                    <div>Región Metropolitana</div>
                 </Row>
                 <Row id="button-profile"><Button variant="outline-primary">GENERAR RUTA</Button></Row>
             </div>
