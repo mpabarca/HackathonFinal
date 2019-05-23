e16382e037e8fa6bdf2f8a5259db2aae19901aff
marianacarbonell <marianacarbonellm@gmail.com>
2019-05-23 16:00:23 -0400

    formulario empresa listo

--- src/components/formulario empresa/Company.js
+++ src/components/formulario empresa/Company.js
@@ -1,43 +1,35 @@
 import React, { Component } from 'react';
 import './Company.css';
+import { Row, Col, Button } from 'react-bootstrap';
 
 class Company extends Component {
 
     render() {
         return (
             <div className="companyBody">
-                <div className="upIcons">
-                    <i class="fas fa-arrow-left"></i>
-                    <p className="back">Volver</p>
-                </div>
-                {/* <div className="textBackground">
-                    <p className="companyText">Comparte tu dato de material reutilizable</p>
-                </div> */}
-                <div className="container">
-                    <i class="far fa-lightbulb"></i>
-                    <input className="input" placeholder="Nombre de la empresa o fábrica"></input>
-                    <div>
-                        <i class="far fa-smile"></i>
-                        <input className="input" placeholder="Que realiza la empresa"></input>
-                    </div>
-                    <div>
-                        <i class="far fa-clock"></i>
-                        <input className="input" placeholder="Horario en el que atienden"></input>
-                    </div>
-                    <div>
-                        <i class="far fa-star"></i>
-                        <input className="input" placeholder="Que material reutilizable tienen"></input>
-                    </div>
-                    <div>
-                        <i class="far fa-envelope"></i>
-                        <input className="input" placeholder="Algún contacto,mail,número,..."></input>
-                    </div>
-                    <div>
-                        <i class="fas fa-map-marker-alt"></i>
-                        <input className="input" placeholder="Dirección"></input>
-                    </div>
-                </div>
-            </div>
+                <Row className="upIcons" ><i id="backIcon"class="fas fa-chevron-left"></i>Volver</Row>
+                <Row className="share">Comparte tu dato de material reutilizable</Row>
+                <Row>
+                    <Col xs="2">
+                        <Row className="iconCompany "><i class="far fa-lightbulb"></i></Row>
+                        <Row className="iconCompany"><i class="far fa-smile"></i></Row>
+                        <Row className="iconCompany"><i class="far fa-clock"></i></Row>
+                        <Row className="iconCompany"><i class="far fa-star"></i></Row>
+                        <Row className="iconCompany"><i class="far fa-envelope"></i></Row>
+                        <Row className="iconCompany"><i class="fas fa-map-marker-alt"></i></Row>
+                    </Col>
+
+                    <Col>
+                        <Row className="inputForm rowMe">Nombre de la empresa o fábrica</Row>
+                        <Row className="inputForm rowMe" >Que realiza la empresa</Row>
+                        <Row className="inputForm rowMe" >Horario en el que atienden</Row>
+                        <Row className="inputForm rowMe" >Que material reutilizable tienen</Row>
+                        <Row className="inputForm rowMe">Algún contacto,mail,número,...</Row>
+                        <Row className="inputForm rowMe" >Dirección</Row>
+
+                    </Col>
+                </Row>
+                <Button variant="outline-primary" className="btnAdd">Agregar</Button> </div>
         )
     }
 }
