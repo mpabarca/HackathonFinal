c82f0f32789eb2fb0a8abe0d19d9f8725900683d
marianacarbonell <marianacarbonellm@gmail.com>
2019-05-23 17:35:00 -0400

    commit para hacer pull

--- src/components/Maps/FilterMarker.js
+++ src/components/Maps/FilterMarker.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import '../Navbar/Navbar.css';
 import Button from 'react-bootstrap/Button';
